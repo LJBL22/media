dec7deafe71badbe924bc35dab9b51f4c15a1412
LJBL22 <yourgitemail@example.com>
2023-11-21 15:33:40 +0800

    U363 render user list (1st)

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -21,7 +21,18 @@ const UsersList = () => {
     return <div>Error fetching data...</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderedUsers = data.map((user) => {
+    // 先示意，之後會成為 component
+    return (
+      <div key={user.id} className='mb-2 border rounded'>
+        <div className='flex p-2 justify-between items-center cursor pointer'>
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{renderedUsers}</div>;
 };
 
 export default UsersList;
