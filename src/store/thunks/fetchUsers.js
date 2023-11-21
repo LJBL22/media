5c32b79b8eae12c0922150a4d2987668ad59db4e
LJBL22 <yourgitemail@example.com>
2023-11-21 11:07:16 +0800

    U360 pause testing by Promise setTimeout

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -6,8 +6,19 @@ const baseUrl = 'http://localhost:3000/users'
 //base type //users/fetch/pending 不會經常出現，是 automatically generated 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const res = await axios.get(baseUrl)
-  console.log(res.data);
+
+  // DEV ONLY!!!
+  await pause(1000)
+
   return res.data // what return here will become payload
 })
+
+// DEV ONLY!!! //helper function
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration) // 寫法注意！
+  })
+}
+
 
 export { fetchUsers }
