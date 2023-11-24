86d4a4e4be2c8d68d53097e707aa6b735b029da7
LJBL22 <yourgitemail@example.com>
2023-11-24 11:14:03 +0800

    U370-1 reusable thunk hook & useCallback

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,34 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, fetchUsers } from '../store';
 import Button from './Button';
 import Skeleton from './Skeleton';
 
-const UsersList = () => {
-  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-  const [loadingUsersError, setLoadingUsersError] = useState(null);
-  const [isCreatingUser, setIsCreatingUser] = useState(false);
-  const [creatingUsersError, setCreatingUsersError] = useState(null);
+const useThunk = (thunk) => {
+  // 以參數 thunk 帶入 fetchUsers/addUsers 即可一個 hook 完成兩件事
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
+
+  const runThunk = useCallback(() => {
+    setIsLoading(true);
+    dispatch(thunk())
+      .unwrap()
+      .catch((err) => setError(err))
+      .finally(() => setIsLoading(false));
+  }, [dispatch, thunk]);
+  //useCallback 用途：當資料有更改，才會更新、回傳『函式』 => 適用於這個抓取資料的 thunk；節省性能
+  // useMemo 是回傳『值』，兩者都是性能優化
+
+  return [runThunk, isLoading, error]; // （1)傳遞出去
+};
+
+const UsersList = () => {
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers); //(2)自動對應
+  const [doAddUser, isCreatingUser, creatingUsersError] = useThunk(addUser);
   const { data } = useSelector((state) => {
     return state.users;
   });
 
   // 精簡版
   useEffect(() => {
-    setIsLoadingUsers(true);
-    dispatch(fetchUsers())
-      .unwrap() // RTK 提供的函式，會解開 Promise
-      .catch((err) => setLoadingUsersError(err))
-      .finally(() => setIsLoadingUsers(false)); // 無論成功失敗，必然會執行的放此
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
   const handleUserAdd = () => {
-    setIsCreatingUser(true);
-    dispatch(addUser())
-      .unwrap()
-      .catch((err) => setCreatingUsersError(err))
-      .finally(() => setIsCreatingUser(false));
+    doAddUser();
   };
 
   if (isLoadingUsers) {
