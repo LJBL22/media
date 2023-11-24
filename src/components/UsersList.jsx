7ef3824e1718a825bfc1aa92756455f77e261895
LJBL22 <yourgitemail@example.com>
2023-11-24 11:39:52 +0800

    refac: show shimmer with title

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -22,35 +22,34 @@ const UsersList = () => {
     doAddUser();
   };
 
+  let content;
   if (isLoadingUsers) {
-    return <Skeleton times={6} className='h-10 w-full' />;
-  }
-
-  if (loadingUsersError) {
-    return <div>Error fetching data...</div>;
-  }
-
-  const renderedUsers = data.map((user) => {
-    // 先示意，之後會成為 component
-    return (
-      <div key={user.id} className='mb-2 border rounded'>
-        <div className='flex p-2 justify-between items-center cursor pointer'>
-          {user.name}
+    content = <Skeleton times={6} className='h-10 w-full' />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data...</div>;
+  } else {
+    content = data.map((user) => {
+      // 先示意，之後會成為 component
+      return (
+        <div key={user.id} className='mb-2 border rounded'>
+          <div className='flex p-2 justify-between items-center cursor pointer'>
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
 
   return (
     <div>
-      <div className='flex flex-row justify-between m-3'>
+      <div className='flex flex-row justify-between items-center rm-3'>
         <h1 className='m-2 text-xl'>Users</h1>
         <Button loading={isCreatingUser} onClick={handleUserAdd}>
           + Add User
         </Button>
         {creatingUsersError && 'Error creating user...'}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 };
