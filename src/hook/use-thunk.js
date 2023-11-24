65326f1ad405730a63004a6d3c596f2c5839ee7d
LJBL22 <yourgitemail@example.com>
2023-11-24 11:17:16 +0800

    U370-2 use-thunk remove to a file

---
+++ src/hook/use-thunk.js
@@ -0,0 +1,21 @@
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+
+export const useThunk = (thunk) => {
+  // 以參數 thunk 帶入 fetchUsers/addUsers 即可一個 hook 完成兩件事
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+
+  const runThunk = useCallback(() => {
+    setIsLoading(true);
+    dispatch(thunk())
+      .unwrap()
+      .catch((err) => setError(err))
+      .finally(() => setIsLoading(false));
+  }, [dispatch, thunk]);
+  //useCallback 用途：當資料有更改，才會更新、回傳『函式』 => 適用於這個抓取資料的 thunk；節省性能
+  // useMemo 是回傳『值』，兩者都是性能優化
+
+  return [runThunk, isLoading, error]; // （1)傳遞出去
+};
