91b55167dc7de90b29f40866f13b84cf3a3fcfb0
LJBL22 <yourgitemail@example.com>
2023-11-25 10:38:15 +0800

    ~U380 think of hierarchy & add ExpandablePanel

--- src/components/UsersListItem.jsx
+++ src/components/UsersListItem.jsx
@@ -2,29 +2,23 @@ import { GoTrash } from 'react-icons/go';
 import { useThunk } from '../hook/use-thunk';
 import { removeUser } from '../store';
 import Button from './Button';
+import ExpandablePanel from './ExpandablePanel';
 
 const UsersListItem = ({ user }) => {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
   const handleUserRemove = () => {
     doRemoveUser(user);
   };
-  return (
-    <div className='mb-2 border rounded'>
-      <div className='flex p-2 justify-between items-center cursor pointer'>
-        <div className='flex flex-row items-center justify-between'>
-          <Button
-            className='mr-3'
-            loading={isLoading}
-            onClick={handleUserRemove}
-          >
-            <GoTrash />
-          </Button>
-          {error && `<div>Error deleting user.${error.message}</div>`}
-          {user.name}
-        </div>
-      </div>
-    </div>
+  const header = (
+    <>
+      <Button className='mr-3' loading={isLoading} onClick={handleUserRemove}>
+        <GoTrash />
+      </Button>
+      {error && `<div>Error deleting user.${error.message}</div>`}
+      {user.name}
+    </>
   );
+  return <ExpandablePanel header={header}>CONTENT!</ExpandablePanel>;
 };
 
 export default UsersListItem;
