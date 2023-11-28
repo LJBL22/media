33661e4f36901b80a15694da5be280c84eca4d64
LJBL22 <yourgitemail@example.com>
2023-11-28 15:13:40 +0800

    ~U390 rendering the list

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,13 +1,36 @@
 import { useFetchAlbumsQuery } from '../store';
+import ExpandablePanel from './ExpandablePanel';
+import Skeleton from './Skeleton';
 
 const AlbumsList = ({ user }) => {
   // 直接命名並印出 result 以看出帶有多少資訊
-  const result = useFetchAlbumsQuery(user); // arg: user, specify the user
-  console.log(result);
+  // const result = useFetchAlbumsQuery(user); // arg: user, specify the user
+  // console.log(result);
 
-  // const { data, error, isLoading } = useFetchAlbumsQuery(user); // arg: user, specify the user
+  const { data, error, isLoading } = useFetchAlbumsQuery(user); // arg: user, specify the user
 
-  return <div>Albums For {user.name}</div>;
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} className='h-10 w-full' />;
+  } else if (error) {
+    content = `<div>Error:${error}</div>`;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel header={header} key={user.id}>
+          list of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums For {user.name}</div>
+      {content}
+    </div>
+  );
 };
 
 export default AlbumsList;
