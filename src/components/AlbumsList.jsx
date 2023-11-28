fd48651d2edaf91d4e8e832b9522097927168a23
LJBL22 <yourgitemail@example.com>
2023-11-28 10:45:10 +0800

    U388 use the generating hook

--- src/components/AlbumsList.jsx
+++ src/components/AlbumsList.jsx
@@ -1,4 +1,8 @@
+import { useFetchAlbumsQuery } from '../store';
+
 const AlbumsList = ({ user }) => {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user); // arg: user, specify the user
+  console.log(data, error, isLoading);
   return <div>Albums For {user.name}</div>;
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    // albums: albumsApi.reducer 同下
+    [albumsApi.reducerPath]: albumsApi.reducer //易於避免失誤
+  },
+  middleware: (getDefaultMiddleware) => { // step 7
+    return getDefaultMiddleware()
+      .concat(albumsApi.middleware);
   }
 })
+
+setupListeners(store.dispatch) // step 7
 
 export * from './thunks/fetchUsers'
 export * from './thunks/addUser'
 export * from './thunks/removeUser'
+
+export { useFetchAlbumsQuery } from './apis/albumsApi' // step 8
