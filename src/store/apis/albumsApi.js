829bf5faa8164d1fcbc751dfd24f0c1c7b5b0052
LJBL22 <yourgitemail@example.com>
2023-11-29 17:36:10 +0800

    U396-397 tag system thoughts

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -2,16 +2,17 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { faker } from "@faker-js/faker";
 import { jsonServerUrl } from '../../hook/use-thunk'
 const albumsApi = createApi({
-  // reducerPath: to store (slice 在 createApi 當下就創建了,因此標注 path 即可，循著 key 找儲存的東西)
   reducerPath: 'album',
-  // baseQuery: how & where to send req
   baseQuery: fetchBaseQuery({
     baseUrl: jsonServerUrl
   }),
-  // endpoints: 每一個行動都要建立，並分為 query & mutation
   endpoints(builder) {
     return {
-      addAlbum: builder.mutation({ // here to auto-generate a hook
+      addAlbum: builder.mutation({
+        // invalidatesTags: ['Album'], // the same as mutation
+        invalidatesTags: (result, error, user) => {
+          return [{ type: 'Album', id: user.id }]
+        },
         query: (user) => {
           return {
             url: '/albums',
@@ -20,7 +21,11 @@ const albumsApi = createApi({
           }
         }
       }),
-      fetchAlbums: builder.query({ // here to auto-generate a hook
+      fetchAlbums: builder.query({
+        // providesTags: ['Album'], // convention: capital letter & no s
+        providesTags: (result, error, user) => {
+          return [{ type: 'Album', id: user.id }]
+        },
         query: (user) => {
           return {
             url: '/albums',
@@ -35,5 +40,5 @@ const albumsApi = createApi({
   }
 })
 
-export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;// this is the hook
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi }
